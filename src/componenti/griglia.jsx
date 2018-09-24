b4ba8c4482ac8ee34bb429bbcdff9d421f1ae88b
marco olivato <1997mrc@gmail.com>
2018-09-24 12:53:09 +0200

    24/09/2018

--- src/componenti/griglia.jsx
+++ src/componenti/griglia.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Grid, Image, Button } from 'semantic-ui-react'
 import img from '../immagini/maglietta.png'
-import link from './prodotto.jsx'
+import prodotto from './prodotto.jsx'
+import { Link, Route, Switch } from 'react-router-dom';
 
 const Griglia = () => (
     <Grid celled>
     <Grid.Row>
       <Grid.Column width={13}>
       <h2>maglietta super figa</h2>
-      <Image src={img} onClick={link} />
+      <Image src={img} onClick={""} />
+      <Link to="/prodotto">Category</Link>
         <p>"descrizione super accattivante"</p>
       </Grid.Column>
       <Grid.Column width={13}>
@@ -22,8 +24,9 @@ const Griglia = () => (
         <p>"descrizione super accattivante3"</p>
       </Grid.Column>
     </Grid.Row>
-
+    <Route path="/prodotto" component={prodotto}/>
   </Grid>
+  
 )
 
 export default Griglia
